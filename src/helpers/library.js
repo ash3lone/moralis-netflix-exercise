c94ac631faf399efccebe974cfe75880b5215221
ash3lone <ash3lone@gmail.com>
2022-04-03 21:08:03 +0100

    initial commit

---
+++ src/helpers/library.js
@@ -0,0 +1,20 @@
+import titanicS from "../images/titanicScene.png";
+import tita from "../images/tit.png";
+import movieLogo from "../images/movieLogo.png";
+
+export const movies = [
+
+{
+
+    Movie:"",
+    Thumbnail: tita,
+    Scene: titanicS,
+    Logo: movieLogo,
+    Name:"Titanic",
+    Description:"Staring Kate Winslet and Leonardo DiCaprio",
+    Genre:"Romance, Drama",
+    Actors:"Kate Winslet, Leonardo DiCaprio",
+    Year:"1998",
+    Duration:"3h 14min",
+},
+];
---
+++ src/pages/Home.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import "./Home.css";
+import { Logo } from '../images/Netflix';
+import {ConnectButton, Icon, TabList, Tab} from 'web3uikit';
+import {movies} from "../helpers/library";
+
+
+const Home = () => {
+
+return(
+  <>
+  <div className="logo">
+    <Logo/>
+  </div>
+  <div className="connect">
+    <Icon file="#ffffff" size={24} svg="bell"/>
+    <ConnectButton/>
+  </div>
+  <div className="topBanner">
+  <TabList defaultActiveKey={1} tabStyle="bar">
+    <Tab tabKey={1} tabName={"Movies"}>
+
+    <div className="scene">
+
+      <img src={movies[0].Scene} className="sceneImg"></img>
+      <img className="scenelogo" src={movies[0].Logo}></img>
+      <p className="sceneDesc">{movies[0].Description}</p>
+    </div>
+    </Tab>
+    <Tab tabKey={2} tabName={"Series"} isDisabled={true}></Tab>
+    <Tab tabKey={3} tabName={"MyList"}></Tab>
+  </TabList>
+  </div>
+  </>
+)
+}
+
+export default Home;
